21e0104000e78c2d60ff386683ddbaebae8cb78e
lujun <lujun5068@gmail.com>
2019-06-08 20:41:23 +0800

    feat: 引入barft-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BraftEditor from 'braft-editor';
+import 'braft-editor/dist/index.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<any> {
+  public render() {
+    return (
+      <div className="App">
+        <BraftEditor />
+      </div>
+    );
+  }
 }
 
 export default App;
